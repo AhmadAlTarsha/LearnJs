6c069303f9c7762d08ca48cf56d998930b311fb5
ahmad_wael <ahmad_wael@digisolfze.com>
2026-02-23 15:20:33 +0300

    update

---
+++ array.js
@@ -0,0 +1,31 @@
+// const numbers = new Array (2,3,5)  this is way to define array in js
+
+
+
+b.push(3);
+
+/* 
+? let a = [1, 2];
+let b = a;
+console.log(a); 
+
+arr1 ──► [1,2]
+arr2 ──┘
+
+
+[1, 2, 3] 
+
+
+!Mutate    تعديل نفس المرجع الموجود بالذاكره
+
+let a = 5;
+let b = a;
+
+b = 10;
+
+console.log(a); // 5
+console.log(b); // 10
+البرمتف بتوخذ نسخه مستقله عشان هيك كل تغيير لا يتاثر بالاخر 
+*/
+
+// السبب انه الثنتين بأشرو ع نفس المرجع لذلك اي تغيير ع اي وحده بتاثر ع  الثانيه 
